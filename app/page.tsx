923b202e293d928a19c764b98120d00345a4f150
Henry Bigalke <henry@studiobigalke.de>
2026-05-26 14:04:28 +0200

    remove: StadtteilReel Section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Nav } from "@/components/landing/Nav";
 import { Hero } from "@/components/landing/Hero";
 import { WhatYouFind } from "@/components/landing/WhatYouFind";
 import { Districts } from "@/components/landing/Districts";
-import { StadtteilReel } from "@/components/landing/StadtteilReel";
 import { ForWhom } from "@/components/landing/ForWhom";
 import { LocalHosts } from "@/components/landing/LocalHosts";
 import { HowWeGrow } from "@/components/landing/HowWeGrow";
@@ -18,7 +17,6 @@ export default function LandingPage() {
       <Hero />
       <WhatYouFind />
       <Districts />
-      <StadtteilReel />
       <ForWhom />
       <LocalHosts />
       <HowWeGrow />
